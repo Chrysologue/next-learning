bc44316417d19a57408bd8fa8f6f2fb38b16b2d5
Chryso <chrysoBeatz@gmail.com>
2026-03-24 14:11:57 +0000

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
-export default function Home(){
+import { posts } from "@/data/posts"
+import Link from "next/link"
 
-  return <h1>This is home page</h1>
+export default function Home() {
+
+  return (<section className="p-10 bg-neutral-200">
+    <div className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-10 text-center md:text-left">
+      {posts.map(post => <Link key={post.slug} className="p-4 shadow-lg" href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
+        <p>{post.description}</p>
+      </Link>)}
+    </div>
+  </section>)
 }
